9a7a3db5404e19916011e6ff406f3f74671300d4
Gloria Lim <glorialimartt@gmail.com>
2024-02-28 10:53:51 -0500

    Add comments

--- filters.rb
+++ filters.rb
@@ -1,16 +1,29 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+# Returns the candidate with that :id
+# If there is no candidate with that id, 
+# it naturally returns nil
 def find(id)
-    # Your code Here
+    
   end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
+# Takes in a single candidate (hash). Note: not the array.
+# Returns true if the candidate has 2 years of experience or more
+# Returns false otherwise
+def experienced?(candidate)
   
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
+end
+
+# Takes in the collection of candidates
+# Returns a subset of the candidates that meet the following criteria:
+  # Are experienced
+  # Have 100 or more Github points
+  # Know at least Ruby or Python
+  # Applied in the last 15 days
+  # Are over the age of 17 (18+)
+def qualified_candidates(candidates)
+  
+end
   
   # More methods will go below
